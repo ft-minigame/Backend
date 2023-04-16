84d434915bbef02dbdbddd2cd78a262d28b27f37
Jonghyun-Joo <jojo@monymony.co>
2023-04-16 17:51:40 +0900

    build: feat file structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './v1/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from './v1/users/domain/repositories/users.repository';
+import { User } from './v1/users/domain/models/user.entity';
+import databaseConfig from './config/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(databaseConfig),
+    TypeOrmModule.forFeature([UserRepository, User]),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
