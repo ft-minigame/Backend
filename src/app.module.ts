7b3c233646fb6d310945ab596ebe2208c6f125a9
남예지 <yum_yeji@nam-yejiui-MacBookPro.local>
2023-04-18 21:05:25 +0900

    fix: module db연결부분 get config로 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cat } from './cat/entity/cat.entity';
+import { CatModule } from './cat/cat.modlue';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+    envFilePath: '.dev.env',
+  }),
+  TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: async (configService: ConfigService) =>{
+      return{
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [Cat],
+        synchronize: true,
+      };
+      },
+    }),
+    CatModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
