acf5c54ef68ddfb993005dea0564d6f70a23a4bd
Jonghyun-Joo <jojo@monymony.co>
2023-05-01 22:28:50 +0900

    chore: remove unused thing

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -4,12 +4,11 @@ import { registerAs } from '@nestjs/config';
 export default registerAs(
   'db',
   (): TypeOrmModuleOptions => ({
-    type: 'mysql',
+    type: 'mariadb',
     host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT) || 32251,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
     synchronize: false,
   }),
