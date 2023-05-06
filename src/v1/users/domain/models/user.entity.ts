43856a6ee8ac17e3937998a5c51c489b0a9c0b5e
남예지 <yum_yeji@nam-yejiui-MacBookPro.local>
2023-05-06 17:46:33 +0900

    Build: feat user.entity.ts

--- src/v1/users/domain/models/user.entity.ts
+++ src/v1/users/domain/models/user.entity.ts
@@ -1,1 +1,32 @@
-export class User {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+export enum UserCoalitions{
+    GUN = "gun",
+    GON = "gon",
+    GAM = "gam",
+    LEE = "lee",
+}
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: string;
+
+    @Column()
+    intraId: string;
+
+    @Column({
+        type: "enum",
+        enum: UserCoalitions,
+    })
+    coalitions: UserCoalitions;
+
+    @Column()
+    character: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
