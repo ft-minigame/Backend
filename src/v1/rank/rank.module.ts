c7acff44eaaf0b4318b2af972d9201f9515f7e6c
남예지 <yum_yeji@nam-yejiui-MacBookPro.local>
2023-05-10 07:11:19 +0900

    Build : create rank.repository.ts

---
+++ src/v1/rank/rank.module.ts
@@ -0,0 +1,13 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Rank } from "./domain/models/rank.entity";
+import { UsersController } from "../users/controllers/users.controller";
+import { RankController } from "./controllers/rank.controller";
+import { RankService } from "./domain/services/rank.service";
+import { RankRepository } from "./domain/repositories/rank.repository";
+
+@Module({
+    imports: [TypeOrmModule.forFeature(Rank)],
+    controllers:[RankController],
+    providers: [RankService, RankRepository],
+})
