111d23314db3bc77765c746da8cdf4fb43a8bd99
남예지 <yum_yeji@nam-yejiui-MacBookPro.local>
2023-05-10 06:53:32 +0900

    Build : create rank.repository.ts

---
+++ src/v1/rank/domain/repositories/rank.repository.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@nestjs/common";
+import { DataSource, Repository } from "typeorm";
+import { Rank } from "../models/rank.entity";
+
+@Injectable()
+export class RankRepository extends Repository<Rank> {
+    constructor(private dataSources: DataSource) {
+        super(Rank, dataSources.createEntityManager());
+    }
+
+    async findAll(): Promise<Rank> {
+        return this.findAll();
+    }
+}
