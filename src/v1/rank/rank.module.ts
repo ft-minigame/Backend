2873f73846d67f8cca81a6c87fe53fd27d6220eb
남예지 <yum_yeji@nam-yejiui-MacBookPro.local>
2023-05-10 07:24:41 +0900

    Fix : remove unused line and add newline

--- src/v1/rank/rank.module.ts
+++ src/v1/rank/rank.module.ts
@@ -1,7 +1,6 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Rank } from "./domain/models/rank.entity";
-import { UsersController } from "../users/controllers/users.controller";
 import { RankController } from "./controllers/rank.controller";
 import { RankService } from "./domain/services/rank.service";
 import { RankRepository } from "./domain/repositories/rank.repository";
@@ -10,4 +9,5 @@ import { RankRepository } from "./domain/repositories/rank.repository";
     imports: [TypeOrmModule.forFeature([Rank])],
     controllers:[RankController],
     providers: [RankService, RankRepository],
-})export class RankModule {}
+})
+export class RankModule {}
