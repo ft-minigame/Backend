8fd162f90211b5eb593a3a48e21911c82fbfd0e5
남예지 <yum_yeji@nam-yejiui-MacBookPro.local>
2023-05-10 06:53:16 +0900

    Build : create rank.service.ts

---
+++ src/v1/rank/domain/services/rank.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from "@nestjs/common";
+import { RankRepository } from "../repositories/rank.repository";
+
+@Injectable()
+export class RankService {
+    constructor(private readonly rankRepository: RankRepository) {}
+
+    async findAll() {
+        return await this.rankRepository.findAll();
+    }
+}
