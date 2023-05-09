4d50c93b185234df2f466cf2f5e32224ed73f4aa
남예지 <yum_yeji@nam-yejiui-MacBookPro.local>
2023-05-10 06:53:02 +0900

    Build : create rank.controller.ts

---
+++ src/v1/rank/controllers/rank.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from "@nestjs/common";
+import { RankService } from "../domain/services/rank.service";
+
+@Controller('rank')
+export class RankController {
+    constructor(private readonly rankService: RankService) {}
+
+    @Get('all')
+    findAll() {
+        return this.rankService.findAll()
+    }
+}
