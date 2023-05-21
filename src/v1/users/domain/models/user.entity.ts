a8e4a8b1ca28c3f83ec9da167af2602c66b1d4c9
남예지 <yum_yeji@nam-yejiui-MacBookPro.local>
2023-05-21 21:03:15 +0900

    fix : add option to intraId

--- src/v1/users/domain/models/user.entity.ts
+++ src/v1/users/domain/models/user.entity.ts
@@ -20,7 +20,7 @@ export class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({ length: 30 })
+  @Column({ length: 30, unique: true })
   intraId: string;
 
   @Column({
@@ -30,7 +30,7 @@ export class User {
   coalitions: UserCoalitions;
 
   @Column()
-  character: number;
+  character: number; // 캐릭터가 추가될 가능성이 있음 => number
 
   @CreateDateColumn()
   createdAt: Date;
