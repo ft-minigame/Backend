90df306596364f9afec4d19d5b9a53a32f6a233b
jooocy <88752693+jooocy@users.noreply.github.com>
2023-05-29 14:19:28 +0900

    feat: create user dtos (#22)
    * build: install class-validator
    
    * refactor: remove unsing props in user.entitiy
    
    * refactor: annotaion in user.entity
    
    * chore: remove annotaion
    
    * refactor: add default value to character column
    
    * feat: add user-create dto
    
    * feat: add update-user dto
    
    * chore: line breack

--- src/v1/users/dto/update-user.dto.ts
+++ src/v1/users/dto/update-user.dto.ts
@@ -1,4 +1,7 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { IsNumber, IsOptional } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  @IsNumber()
+  @IsOptional()
+  readonly character: number;
+}
