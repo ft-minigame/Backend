26acb82ccfaaafb63811d63d16bcd84d4180af5e
Jonghyun-Joo <jojo@monymony.co>
2023-05-29 20:29:44 +0900

    build: cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'https://web-ft-mingame-6g2llf3p55qu.sel3.cloudtype.app/*',
+      'http://localhost:3000/*',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(8080);
 }
