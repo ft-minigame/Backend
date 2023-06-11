60dbd4e30cfdde1bd818a3f26c7a335cc90f2fa5
남예지 <yum_yeji@nam-yejiui-MacBookPro.local>
2023-06-11 19:39:34 +0900

    fix: remove update and add find User method

--- src/v1/game/domain/services/game.service.ts
+++ src/v1/game/domain/services/game.service.ts
@@ -2,17 +2,21 @@ import { Injectable } from '@nestjs/common';
 import { GameRepository } from '../repositories/game.repository';
 import { Game } from '../models/game.entity';
 import { CreateGameDto } from '../../dto/create-game.dto';
-import { UpdateGameDto } from '../../dto/update-game.dto';
+import { UserRepository } from 'src/v1/users/domain/repositories/users.repository';
 
 @Injectable()
 export class GameService {
-  constructor(private readonly gameRepository: GameRepository) {}
-  async createAndSave(): Promise<Game> {
-    const createGameDto = new CreateGameDto();
-    return await this.gameRepository.createAndSave(createGameDto);
-  }
-
-  async updateById(id: string, updateGameDto: UpdateGameDto): Promise<Game> {
-    return await this.gameRepository.updateById(id, updateGameDto);
+  constructor(
+    private readonly gameRepository: GameRepository,
+    private readonly userRepository: UserRepository,
+  ) {}
+  async createAndSave(createGameDto: CreateGameDto): Promise<Game> {
+    const user = await this.userRepository.findOneBy({
+      intraId: createGameDto.intraId,
+    });
+    if (!user) {
+      throw new Error('User not found');
+    }
+    return await this.gameRepository.createAndSave(createGameDto, user);
   }
 }
