4fdde24760dc6b6c8f3d67b6d1505751714342ef
남예지 <yum_yeji@nam-yejiui-MacBookPro.local>
2023-06-11 19:40:04 +0900

    fix: remove update

--- src/v1/game/domain/repositories/game.repository.ts
+++ src/v1/game/domain/repositories/game.repository.ts
@@ -1,22 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { Game } from '../models/game.entity';
 import { DataSource, Repository } from 'typeorm';
+import { CreateGameDto } from '../../dto/create-game.dto';
+import { User } from 'src/v1/users/domain/models/user.entity';
 
 @Injectable()
 export class GameRepository extends Repository<Game> {
   constructor(private gameSources: DataSource) {
     super(Game, gameSources.createEntityManager());
   }
-  g;
 
-  async createAndSave(gameData: Partial<Game>): Promise<Game> {
-    const game = this.create(gameData);
+  async createAndSave(createGameDto: CreateGameDto, user: User): Promise<Game> {
+    const game = this.create(createGameDto);
+    if (!game) {
+      throw new Error('Game not created!');
+    }
+    game.user = user;
     await this.save(game);
     return game;
-  }
-
-  async updateById(id: string, updateData: Partial<Game>): Promise<Game> {
-    await this.update(id, updateData);
-    return this.findOneBy({ id });
   }
 }
