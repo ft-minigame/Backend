8a45a0be95bea063e539b60e48eb2cf119ef152e
남예지 <yum_yeji@nam-yejiui-MacBookPro.local>
2023-06-11 20:37:23 +0900

    fix: move find User method to controller from repository

--- src/v1/game/domain/services/game.service.ts
+++ src/v1/game/domain/services/game.service.ts
@@ -3,20 +3,13 @@ import { GameRepository } from '../repositories/game.repository';
 import { Game } from '../models/game.entity';
 import { CreateGameDto } from '../../dto/create-game.dto';
 import { UserRepository } from 'src/v1/users/domain/repositories/users.repository';
+import { User } from 'src/v1/users/domain/models/user.entity';
 
 @Injectable()
 export class GameService {
-  constructor(
-    private readonly gameRepository: GameRepository,
-    private readonly userRepository: UserRepository,
-  ) {}
-  async createAndSave(createGameDto: CreateGameDto): Promise<Game> {
-    const user = await this.userRepository.findOneBy({
-      intraId: createGameDto.intraId,
-    });
-    if (!user) {
-      throw new Error('User not found');
-    }
+  constructor(private readonly gameRepository: GameRepository) {}
+
+  async createAndSave(createGameDto: CreateGameDto, user: User): Promise<Game> {
     return await this.gameRepository.createAndSave(createGameDto, user);
   }
 }
