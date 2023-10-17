6af92869b4b47dca907e83d9aacafbdcd0ea9e48
jooocy <88752693+jooocy@users.noreply.github.com>
2023-10-17 23:58:16 +0900

    Revert "refactor: change user's repository"

--- src/v1/game/dto/create-game.dto.ts
+++ src/v1/game/dto/create-game.dto.ts
@@ -1,4 +1,10 @@
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import {
+  IsBoolean,
+  IsDate,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+} from 'class-validator';
 
 export class CreateGameDto {
   @IsNotEmpty()
